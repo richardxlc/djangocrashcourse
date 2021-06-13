9fc448cfeb08d8b0eed5ac47b089fa0eb13e1f9b
richardxlc <86317810@163.com>
2021-06-13 15:40:08 +0800

    crashcourse

---
+++ crashcourse/todo/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .views import (todo_list,
+                    todo_detail,
+                    todo_create,
+                    todo_update,
+                    todo_delete)
+
+app_name = "todos"
+
+urlpatterns=[
+    path('',todo_list),
+    path('create/',todo_create),
+    path('<int:id>/',todo_detail),
+    path('<int:id>/update/',todo_update),
+    path('<int:id>/delete/',todo_delete),
+]
